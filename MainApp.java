9c5c1c656e45d1df840d96c74aa1d0993719558d
phucnguyen117 <144077716+phucnguyen117@users.noreply.github.com>
2023-10-03 09:16:55 +0700

    Add files via upload

---
+++ MainApp.java
@@ -0,0 +1,44 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		/**
+		 * Khai báo biến
+		 */
+		String firstName =  "Hoang Huu Phuc";
+		String lastName = "Nguyen";
+		int maSinhVien = 1234;
+		long cmnd = 12345679;
+		String className = "23CDTH41";
+		String nameSchool = "HueIC";
+		float dtb = 5.0f;
+		String name = "Hoang N";
+		String diaChi = "93 Hung Vuong";
+		long sdt = 123456789;
+		float hsl = 5000000f;
+		String nameCar = "Tesla Model S";
+		String hang = "Tesla";
+		float gia = 3000000000f;
+		String color = "white";
+		System.out.println("Ho va ten");
+		System.out.println(firstName + "Hoang Huu Phuc Nguyen" + lastName);
+		System.out.println("1234" + maSinhVien);
+		System.out.println("01234579" + cmnd);
+		System.out.println("23CDTH41" + className);
+		System.out.println("HueIC" + nameSchool);
+		System.out.println("5.0f" + dtb);
+		//TTCN
+		System.out.println("Hoang N" + name);
+		System.out.println("93 Hung Vuong" + diaChi);
+		System.out.println("1234567890" + sdt);
+		System.out.println("5000000f" + hsl);
+		//TTXe
+		System.out.println("Tesla Model S" + nameCar);
+		System.out.println("Tesla" + hang);
+		System.out.println("3000000000f" + gia);
+		System.out.println("white" + color);
+		
+	}
+
+}
